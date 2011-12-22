b74269cc3633c48a2734bd18a3c06455eb68eeca
Barosl LEE <git@barosl.com>
2011-12-22 15:16:24 +0900

    pygraph + graphviz -> networkx + matplotlib

--- lr-tabler.py
+++ lr-tabler.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
 
-from pygraph.classes.graph import graph
-from pygraph.readwrite.dot import write as write_graph
-
-import sys
-sys.path.append('..')
-sys.path.append('/usr/lib/graphviz/python/')
-sys.path.append('/usr/lib64/graphviz/python/')
-import gv
+import matplotlib; matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import networkx as nx
 
 rules = {
 	'S\'': [['G']],
@@ -15,12 +10,30 @@ rules = {
 	'E': [['E', '+', 'T'], 'T'],
 	'T': [['T', '*', 'f'], 'f'],
 }
+st_sym = 'S\''
 
 rules = {
 	'ACCEPT': [['LIST']],
 	'LIST': [['LIST', ',', 'ELEMENT'], ['ELEMENT']],
 	'ELEMENT': [['a']],
 }
+st_sym = 'ACCEPT'
+
+rules = {
+	'E\'': [['E']],
+	'E': [['E', '+', 'T'], ['T']],
+	'T': [['T', '*', 'F'], ['F']],
+	'F': [['(', 'E', ')'], ['id']],
+}
+st_sym = 'E\''
+
+rules = {
+	'S\'': [['S']],
+	'S': [['L', '=', 'R'], ['R']],
+	'L': [['*', 'R'], ['id']],
+	'R': [['L']],
+}
+st_sym = 'S\''
 
 class Item:
 	st = None
@@ -67,13 +80,21 @@ def get_closure(items):
 def get_goto(items, sym):
 	return get_closure(Item(x.st, x.syms, x.idx+1) for x in items if x.idx != len(x.syms) and x.syms[x.idx] == sym)
 
+def get_items_str(items):
+	res = ''
+
+	for item in items:
+		res += repr(item)+'\n'
+
+	return res.rstrip()
+
 def main():
 	item_grps = []
-	parent_grps = {}
-	child_grps = {}
+	parent_grp_grps = {}
+	child_grp_grps = {}
 	repr2items = {}
 
-	items = get_closure([Item('ACCEPT', rules['ACCEPT'][0], 0)])
+	items = get_closure([Item(st_sym, rules[st_sym][0], 0)])
 	repr2items[repr(items)] = items
 
 	que = [items]
@@ -97,11 +118,11 @@ def main():
 			if found_items: new_items = found_items
 			else: repr2items[new_items_repr]  = new_items
 
-			if new_items_repr not in parent_grps: parent_grps[new_items_repr] = []
-			parent_grps[new_items_repr].append(items)
+			if new_items_repr not in parent_grp_grps: parent_grp_grps[new_items_repr] = []
+			parent_grp_grps[new_items_repr].append([items, sym])
 
-			if items_repr not in child_grps: child_grps[items_repr] = []
-			child_grps[items_repr].append(new_items)
+			if items_repr not in child_grp_grps: child_grp_grps[items_repr] = []
+			child_grp_grps[items_repr].append([new_items, sym])
 
 			if new_items != found_items: que.append(new_items)
 
@@ -110,19 +131,29 @@ def main():
 			print item
 		print '----'
 
-	for items_repr, childs in child_grps.iteritems():
+	for items_repr, child_grps in child_grp_grps.iteritems():
 		items = repr2items[items_repr]
-		for child in childs:
-			print '* %s => %s' % (items, child)
+		for childs, sym in child_grps:
+			print '* %s => %s' % (items, childs)
 
-	gr = graph()
-	gr.add_nodes(['a', 'b', 'c'])
-	gr.add_edge(('a', 'b'))
+	gr = nx.DiGraph()
 
-	dot = write_graph(gr)
-	gvv = gv.readstring(dot)
-	gv.layout(gvv, 'dot')
-	gv.render(gvv, 'png', 'test.png')
+	for items_repr, child_grps in child_grp_grps.iteritems():
+		items = repr2items[items_repr]
+
+		items_str = get_items_str(items)
+
+		for childs, sym in child_grps:
+			childs_str = get_items_str(childs)
+			gr.add_edge(items_str, childs_str, label=sym)
+
+	pos = nx.spring_layout(gr)
+	nx.draw(gr, pos)
+	nx.draw_networkx_edge_labels(gr, pos, node_size=100)
+	plt.axis('off')
+	fig = plt.gcf()
+	fig.set_size_inches(10, 10)
+	plt.savefig('test.png')
 
 if __name__ == '__main__':
 	main()
